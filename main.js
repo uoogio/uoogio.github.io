f740ad15638ec69211f8a68c7ccb14523b25a48b
uoogio <giovanni.buono12@gmail.com>
2020-05-15 15:57:05 +0200

    mod

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+var connectionString = 'HostName=assignment4.azure-devices.net;DeviceId=MyNodeDevice;SharedAccessKey=axkIDEbJ+9SXl85TsQTpBhFbqDLDXMjw/45e6+e51Yg=';
+
+// Using the Node.js Device SDK for IoT Hub:
+//   https://github.com/Azure/azure-iot-sdk-node
+// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message;
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
 let alert = '<div class="alert alert-warning" role="alert">'+
   'Seems like if your device/browser do not support accelerometer. Try to change device/browser.'+
   '</div>';
@@ -10,6 +19,22 @@ $(document).ready(function() {
     $('#stop').click( () =>{sensor.stop()} );
     $('#start').click( () =>{sensor.start()} );
     
+    var temperature = 20 + (Math.random() * 15);
+  var message = new Message(JSON.stringify({
+    temperature: temperature,
+    humidity: 60 + (Math.random() * 20)
+  }));
+  console.log('Sending message: ' + message.getData());
+
+  // Send the message.
+  client.sendEvent(message, function (err) {
+    if (err) {
+      console.error('send error: ' + err.toString());
+    } else {
+      console.log('message sent');
+    }
+  });
+  
     sensor.onreading = () => {
         console.log("Acceleration along X-axis: " + sensor.x);
         $('#x').text(sensor.x);
