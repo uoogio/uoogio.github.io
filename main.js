240f38c2744e5058560793919b134119d95bdbff
uoogio <giovanni.buono12@gmail.com>
2020-05-21 12:34:08 +0200

    mod

--- main.js
+++ main.js
@@ -109,6 +109,15 @@ $(document).ready(async function () {
 
           // Specify the method
           method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          redirect: 'follow',
+          referrerPolicy: 'no-referrer',
+          //body: JSON.stringify(data)
 
           // A JSON payload
           body: JSON.stringify({
